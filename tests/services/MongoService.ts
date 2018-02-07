51b28f5b3ac4e5e13c520da907116245ccef4da4
VojtechStep <vojtechstepancik@outlook.com>
2018-02-07 16:23:23 +0100

    Better tests for MongoService

--- tests/services/MongoService.ts
+++ tests/services/MongoService.ts
@@ -1,22 +1,30 @@
 import generalTest, { RegisterContextual } from 'ava';
 import DBMemoryServer from 'mongodb-memory-server';
+import { MongoService } from '../../lib';
+
+interface UserMetadata {}
 
 const test = generalTest as RegisterContextual<{
   mongo: {
     getConnectionString(db?: string): Promise<string>;
     stop(): void;
   };
+  userService?: MongoService<UserMetadata>;
 }>;
 
-test.beforeEach('Start in-memory mongod', async t => {
+test.beforeEach('Start in-memory mongod', t => {
   t.context = {
     mongo: new DBMemoryServer(),
   };
 });
 
+test.beforeEach('Connect service to MongoDB instance', async t => {
+  const uri = await t.context.mongo.getConnectionString();
+  t.context.userService = await new MongoService(uri).connect();
+});
+
 test('Get connection', async t => {
-  const db = await t.context.mongo.getConnectionString();
-  t.truthy(db);
+  t.true(t.context.userService!.isConnected(''));
 });
 
 test.todo('Create user');
